beadb4dde4aefaf0b24fa4f907a67c5b71497a23
Serg8787 <tsybulnikserg@gmail.com>
2021-08-04 11:52:07 +0300

    add gif bitcoin

--- app/src/main/java/com/example/bitcoin/API.kt
+++ app/src/main/java/com/example/bitcoin/API.kt
@@ -2,11 +2,12 @@ package com.example.bitcoin
 
 import retrofit2.Call
 import retrofit2.http.GET
+import java.util.*
 
 interface API {
 
-    @GET("v1/bpi/currentprice.json/")
-    fun getTime(): Call<Time>
+//    @GET("v1/bpi/currentprice.json/")
+//    fun getTime(): Call<Time>
 
     @GET("v1/bpi/currentprice.json/")
     fun getBitcoin(): Call<Bitcoin>
--- app/src/main/java/com/example/bitcoin/MainActivity.kt
+++ app/src/main/java/com/example/bitcoin/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.bitcoin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -13,36 +14,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Glide
+            .with(this)
+            .load(R.drawable.icons8_bitcoin_gif)
+            .into(ivBitcoin);
+
+
 
         val retrofit = RetrofitClient.getClient("https://api.coindesk.com/").create(API::class.java)
         retrofit.getBitcoin().enqueue(object : Callback<Bitcoin> {
             override fun onResponse(call: Call<Bitcoin>, response: Response<Bitcoin>) {
-                if (response.body() != null){
-                    tvTime.text =  response.body()?.chartName
-                    tvDiclaimer.text = response.body()?.disclaimer
-                    Log.d("MyLOG", "onRespose"+response.body()?.chartName)
+                if (response.body() != null) {
+                    tvTime.text = response.body()?.chartName
+                    // не нужен, про безопасность или куда то вниз
+//                    tvDiclaimer.text = response.body()?.disclaimer
+//
+                    tvEURcode.text = response.body()?.bpi?.EUR?.code
+                    tvEURdescription.text = response.body()?.bpi?.EUR?.description
+                    tvEURrate.text = response.body()?.bpi?.EUR?.rate
+                    tvEURrate_float.text = response.body()?.bpi?.EUR?.rate_float.toString()
+                    tvEURsymbol.text = response.body()?.bpi?.EUR?.symbol
+
+                    Log.d("MyLOG", "onRespose" + response.body()?.chartName)
                 }
             }
+
             override fun onFailure(call: Call<Bitcoin>, t: Throwable) {
-                Log.d("MyLOG", "Fauler")
-            }
-        }
-        )
-
-
-        retrofit.getEUR().enqueue(object : Callback<EUR> {
-            override fun onResponse(call: Call<EUR>, response: Response<EUR>) {
-                if (response.body() != null){
-                    tvEURcode.text =  response.body()?.code
-//                    tvEURdescription.text = response.body()?.description
-                    tvEURrate.text = response.body()?.rate
-                    tvEURrate_float.text = response.body()?.rate_float.toString()
-//                    tvEURsymbol.text = response.body()?.symbol
-                    Log.d("MyLOG", "onRespose")
-                }
-            }
-
-            override fun onFailure(call: Call<EUR>, t: Throwable) {
                 Log.d("MyLOG", "Fauler")
             }
         }
